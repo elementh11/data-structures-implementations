553a6e3bec231ad8abc4b8df404b6c54698f350a
Adrian Popescu <elementh11@gmail.com>
2020-01-22 13:39:18 -0500

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,29 @@
+def quicksort(arr, left, right):
+
+    if left >= right:
+        return
+
+    pivot = arr[(left + right)/2]
+
+    index = partition(arr, left, right, pivot)
+
+    quicksort(arr, left, index - 1)
+    quicksort(arr, index, right)
+
+def partition(arr, left, right, pivot):
+
+    while(left <= right):
+        while(arr[left] < pivot):
+            left += 1
+
+        while(arr[right] > pivot):
+            right -= 1
+
+        if (left <= right):
+            #swap(arr, left, right)
+            arr[left], arr[right] = arr[right], arr[left]
+            
+            left += 1
+            right -= 1
+
+    return left
